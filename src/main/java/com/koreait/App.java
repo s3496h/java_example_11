5630421efa0df3805d641073ac5d5af845905891
s3496h <s3496h@gmail.com>
2024-11-04 14:56:05 +0900

    핵심기능 이전

---
+++ src/main/java/com/koreait/App.java
@@ -0,0 +1,35 @@
+package com.koreait;
+
+import java.util.Scanner;
+
+public class App {
+    private Scanner sc;
+    private TodoController todoController;
+    private SystemController systemController;
+
+    public App() {
+        sc = new Scanner(System.in);
+        todoController = new TodoController();
+        systemController = new SystemController();
+    }
+
+    public void run() {
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
+            if (cmd.equals("exit")) {
+                systemController.exit();
+                break;
+            } else if (cmd.equals("add")) {
+                todoController.add();
+            } else if (cmd.equals("list")) {
+                todoController.list();
+            } else if (cmd.equals("del")) {
+                todoController.del();
+            } else if (cmd.equals("modify")) {
+                todoController.modify();
+            }
+        }
+        sc.close();
+    }
+}
